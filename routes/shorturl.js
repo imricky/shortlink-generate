8a8565b340389164a52a410bc979fa48b1e2b038
rqcao <rqcao@outlook.com>
2019-07-24 20:23:34 +0800

    :leaves:优化代码逻辑，根据短链接查找长链接，准备用于跳转

--- routes/shorturl.js
+++ routes/shorturl.js
@@ -13,6 +13,9 @@ const getAsync = promisify(client.get).bind(client);
 const SHORT_LINK_PREFIX = 'expressShortlink';
 const EXPIRE_TIME = 60 * 60 * 24 * 365; //过期时间存成1年
 
+router.get('/', (req, res, next) => {
+  res.send('接口get成功了')
+})
 
 router.post('/', async (req, res, next) => {
   let userURL = req.body.userURL;
@@ -20,14 +23,18 @@ router.post('/', async (req, res, next) => {
     code: 200,
     success: true,
     message: '成功',
-    short_link: null
+    short_link: null,
+    long_link: userURL
   }
-  let genLinkInDatabase = await isExistShortlink(userURL);
+  let shortLinkInDatabase = await isExistShortlink(userURL);
   //如果数据库里已经有长网址了，直接返回短网址结果
-  if (genLinkInDatabase !== "") {
-    returnObj.short_link = genLinkInDatabase;
-    res.json(returnObj);
-    return;
+  if (shortLinkInDatabase !== null) {
+    return res.json({
+      code: 204,
+      message: '短链接已存在',
+      short_link: shortLinkInDatabase,
+      long_link: userURL
+    });
   }
   //生成短链接
   let maxPhid = await GetMaxPhid();
@@ -39,21 +46,21 @@ router.post('/', async (req, res, next) => {
   //插入数据库
   let success = await storeShortLink(params);
   if (!success) {
-    res.json({
+    return res.json({
       code: 500,
       success: false,
       message: '数据库插入错误',
-      short_link: null
     })
-    return;
   }
-  returnObj.short_link = short_link;
-  res.json(returnObj);
+  res.json({
+    code: 200,
+    message: '成功',
+    short_link: short_link,
+    long_link: userURL
+  })
 })
 
-router.get('/', (req, res, next) => {
-  res.send('接口get成功了')
-})
+
 
 //10进制转62进制
 //3843 对应61*62^0+61*62^1    3844对应100
@@ -80,12 +87,11 @@ async function GetMaxPhid() {
 }
 
 //查找数据库是否存在对应的短网址是否存在。
-//先去redis里查，没有查到就去mysql里查，同时同步redis,都没有的话再生成一个新的
 async function isExistShortlink(long_link) {
   let sql = 'select * from fg_shortlink where long_link = ?'
   let result = await query(sql, [long_link]);
   if (result.length === 0) {
-    return '';
+    return null;
   } else {
     return result[0].short_link
   }
@@ -114,11 +120,30 @@ async function storeShortLink(params) {
   }
   //存redis
   if (success === true) {
-    client.set('key', 'value!', 'EX', 10);
     let resultRedis = await setAsync(`${SHORT_LINK_PREFIX}##${short_link}`, long_link, 'EX', EXPIRE_TIME)
     success = resultRedis === "OK"
   }
   return success
+}
+
+
+//查找长链接，用于跳转
+async function queryLink(short_link) {
+  //先去redis里查
+  let resRedis = await getAsync(`${SHORT_LINK_PREFIX}##${short_link}`);
+  if (resRedis !== null) {
+    return resRedis
+  }
+  //redis里没找到就去找MySQL
+  let resMySQL = await query('select long_link from fg_shortlink where short_link = ?', [short_link]);
+  if (resMySQL && resMySQL.length > 0) {
+    const long_link = resMySQL[0].long_link;
+    //同步redis
+    await setAsync(`${SHORT_LINK_PREFIX}##${short_link}`, long_link, 'EX', EXPIRE_TIME);
+    return long_link;
+  }
+
+  return null; //找不到返回空
 }
 
 module.exports = router
