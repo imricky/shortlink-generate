98718a1a9a08869262bb5200899fa63040da595e
rqcao <rqcao@outlook.com>
2019-07-30 21:58:57 +0800

    🌸 实现了注册和登录的功能

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var session = require('express-session')
+var redisStore = require('connect-redis')(session)
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var shorturlRouter = require('./routes/shorturl');
-var jumpurlRouter = require('./routes/jumpurl');
 
-var app = express();
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var shorturlRouter = require('./routes/shorturl')
+var jumpurlRouter = require('./routes/jumpurl')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var app = express()
 
-app.set('views',path.join(__dirname , 'views') );
-app.engine('.html', require('ejs').__express);  
-app.set('view engine', 'html'); 
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser('yog'))
+// app.use(session({
+//   'resave': true,
+//   'secret': 'yog',    // 签名，与上文中cookie设置的签名字符串一致
+//   'cookie': {
+//     'maxAge': 90000,
+//     'user':'test'
+//   },
+//   'name': 'session_id' //  在浏览器中生成cookie的名称key，默认是connect.sid
+// }));
+// session
+app.use(session({
+  name: 'session_id', // 这里是cookie的name，默认是connect.sid
+  secret: 'yog', // 建议使用 128 个字符的随机字符串
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    'maxAge': 1000*60,
+    'httpOnly':false,
+  },
+  store: new redisStore({
+    host: '127.0.0.1',
+    port: '6379',
+    db: 0,
+    pass: '',
+  })
+}))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/s', jumpurlRouter);
-app.use('/users', usersRouter);
-app.use('/su', shorturlRouter);
 
-app.get('*', (req, res) => {   
-  res.render('404', {  
-      title: '404 Not Found',  
-  });  
-})  
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.html', require('ejs').__express)
+app.set('view engine', 'html')
 
-module.exports = app;
+app.use('/', indexRouter)
+app.use('/s', jumpurlRouter)
+app.use('/users', usersRouter)
+app.use('/su', shorturlRouter)
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404 Not Found',
+  })
+})
+
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,116 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const mysql = require('mysql')
+const query = require('../util/dbhelper')
+const redis = require("redis"),
+    client = redis.createClient();
+
+const { promisify } = require('util');
+const setAsync = promisify(client.set).bind(client);
+const getAsync = promisify(client.get).bind(client);
+
+
+const crypto = require('crypto');
+crypto.DEFAULT_ENCODING = 'hex';
+
+const pbkdf2Async = promisify(crypto.pbkdf2).bind(crypto);
+pbkdf2Async('secret', 'qwertyu', 100000, 512, 'sha512', (err, derivedKey) => {
+  if (err) throw err;
+  console.log(derivedKey);  // '3745e48...aa39b34'
 });
 
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express'})
+})
 
-module.exports = router;
+router.get('/login', function (req, res, next) {
+  if(req.session.user){
+    res.redirect('/');
+    return;
+  }
+  res.render('login', {title: 'Express'})
+})
+
+router.get('/register', function (req, res, next) {
+  res.render('register', {title: 'Express'})
+})
+
+
+router.post('/login', async (req, res, next) => {
+  let {user, password} = req.body;
+  let sql = await query('select * from fg_user where user = ?',[user]);
+  if(sql.length === 0){
+    return res.json({
+      success:false,
+      code:500,
+      errorMsg:'用户名不存在',
+    })
+  }
+  let userInDB = sql[0].user;
+  let passwordInDB = sql[0].password;
+  //加盐做对比
+  let cryptoPassword = await pbkdf2Async(password, 'qwertyu', 100000, 512, 'sha512');
+  if(cryptoPassword !== passwordInDB){
+    return res.json({
+      success:false,
+      code:500,
+      errorMsg:'密码错误',
+    })
+  }
+  //存入session信息
+  req.session.user  = 'ricky'
+  res.json({
+    success:true,
+    code:200,
+    errorMsg:'',
+  })
+
+})
+
+router.post('/register', async (req, res, next) => {
+  let {user, password} = req.body;
+  try {
+    let sqlUserExist = await query('select * from fg_user where user = ?', [user]);
+    if(sqlUserExist.length !== 0){
+      return res.json({
+        success:false,
+        code:500,
+        errorMsg:'用户名重复，亲再重新选一个吧~',
+      })
+    }
+
+    let cryptoPassword = await pbkdf2Async(password, 'qwertyu', 100000, 512, 'sha512');
+    let post = {
+      user:user,
+      password:cryptoPassword
+    };
+    let sql = await query('INSERT INTO fg_user SET ?', post);
+    if (sql.affectedRows === 1) {
+      return res.json({
+        success:true,
+        code:200,
+        errorMsg:'',
+        user:user
+      })
+    }else {
+      return res.json({
+        success:false,
+        code:500,
+        errorMsg:'保存失败，原因是数据库插入错误',
+      })
+    }
+  }catch (e) {
+    res.json({
+      success:false,
+      code:500,
+      errorMsg:e,
+    })
+  }
+
+
+})
+
+
+module.exports = router
